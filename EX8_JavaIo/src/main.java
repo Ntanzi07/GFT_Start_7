ad91d1428a414024528fb76ccd7177c16fa2a928
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-07-28 16:19:40 -0300

    primeira parte java.io

---
+++ EX8_JavaIo/src/main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import br.com.dio.persistence.FilePersistence;
+import br.com.dio.persistence.IOFilePersistence;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        FilePersistence persistence = new IOFilePersistence("user.csv");
+        System.out.println("==================Escrevendo==================");
+        System.out.println(persistence.write("Nathan;nathan@gmail.com;03/07/2003;"));
+        System.out.println(persistence.write("Julia;julia@gmail.com;20/10/2004;"));
+        System.out.println(persistence.write("Maud;maud@gmail.com;04/11/2008;"));
+        System.out.println("==================Lendo==================");
+        System.out.println(persistence.findAll());
+        System.out.println("==================Lendo Apenas==================");
+        System.out.println(persistence.findBy("Nathan"));
+        System.out.println(persistence.findBy("julia"));
+        System.out.println(persistence.findBy("2008"));
+    }
+}
